aa5d5e79d061f6692825b5f8d3f3934f711db5b4
Едвін <edvinfaas@gmail.com>
2023-08-16 18:28:53 +0300

    console.log

--- Lesson-1/main.js
+++ Lesson-1/main.js
@@ -1,44 +1,83 @@
+//******************** Task 1 ********************//
 let a1
 a1 = 'Hello'
+console.log(a1);
 let a2
 a2 = 'owu'
+console.log(a2);
 let a3
 a3 = 'com'
+console.log(a3);
 let a4
 a4 = 'ua'
+console.log(a4);
 let a5
 a5 = 1
+console.log(a5);
 let a6
 a6 = 10
+console.log(a6);
 let a7
 a7 = -999
+console.log(a7);
 let a8
 a8 = 123
+console.log(a8);
 let a9
 a9 = 3.14
+console.log(a9);
 let a10
 a10 = 2.7
+console.log(a10);
 let a11
 a11 = 16
+console.log(a11);
 let a12
 a12 = true
+console.log(a12);
 let a13
 a13 = false
-console.log(a1)
-console.log(a2)
-console.log(a3)
-console.log(a4)
-console.log(a5)
-console.log(a6)
-console.log(a7)
-console.log(a8)
-console.log(a9)
-console.log(a10)
-console.log(a11)
-console.log(a12)
-console.log(a13)
+console.log(a13);
 
-/****************************************************************************************************/
+//******************** Task 2 ********************//
+
+let firstName
+firstName = "Varga "
+console.log(firstName);
+let middleName
+middleName = "Edwin "
+console.log(middleName);
+let lastName
+lastName = "Stepanovych "
+console.log(lastName);
+let person
+person = firstName+middleName+lastName
+console.log(person);
+
+//******************** Task 3 ********************//
+
+let a
+a = 100
+console.log(a)
+console.log(typeof a);
+let b
+b = '100'
+console.log(b)
+console.log(typeof b);
+let c
+c = true
+console.log(true);
+console.log(typeof true);
+
+//******************** Task 4 ********************//
+
+let age
+age = 29
+prompt(middleName)
+prompt(lastName)
+prompt(age)
+console.log(middleName+lastName+age)
+
 
 
 
