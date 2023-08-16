4e75fb3e342fcc9a2718350a2a5b2fb64dffda38
Едвін <edvinfaas@gmail.com>
2023-08-16 17:19:43 +0300

    console.log

---
+++ Lesson-1/main.js
@@ -0,0 +1,45 @@
+let a1
+a1 = 'Hello'
+let a2
+a2 = 'owu'
+let a3
+a3 = 'com'
+let a4
+a4 = 'ua'
+let a5
+a5 = 1
+let a6
+a6 = 10
+let a7
+a7 = -999
+let a8
+a8 = 123
+let a9
+a9 = 3.14
+let a10
+a10 = 2.7
+let a11
+a11 = 16
+let a12
+a12 = true
+let a13
+a13 = false
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+console.log(a5)
+console.log(a6)
+console.log(a7)
+console.log(a8)
+console.log(a9)
+console.log(a10)
+console.log(a11)
+console.log(a12)
+console.log(a13)
+
+/****************************************************************************************************/
+
+
+
+
